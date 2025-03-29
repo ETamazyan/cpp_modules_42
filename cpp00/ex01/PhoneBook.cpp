54c2d9db7644110ea5f859f50ccdaa9148735627
E1-Tamazyan <el.tamazyan03@gmail.com>
2025-03-29 19:22:24 +0400

    ex01 - left to print the concat's info

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,5 +1,44 @@
 #include "PhoneBook.hpp"
 
+
+
+// *********************
+// *********************
+#include <iostream>
+#include <string>
+#include <climits>  // For INT_MAX and INT_MIN
+
+int my_stoi(const std::string& str)
+{
+	int result = 0;
+	int sign = 1;
+	size_t i = 0;
+	char ch;
+	int digit;
+
+	if (str.empty())
+		return 0;
+	while (i < str.size() && (str[i] == ' ' || str[i] == '\t'))
+		i++;
+	if (i < str.size() && (str[i] == '-' || str[i] == '+')) {
+		sign = (str[i] == '-') ? -1 : 1;
+		i++;
+	}
+	for (; i < str.size(); i++)
+	{
+		ch = str[i];		
+		if (ch < '0' || ch > '9')
+			return 0;
+		digit = ch - '0';
+		if (result > (INT_MAX - digit) / 10)
+			return 0;
+		result = result * 10 + digit;
+	}
+	return (sign * result);
+}
+
+// *********************
+// *********************
 PhoneBook::PhoneBook()
 {
 	this->contacts_amount = 0;
@@ -40,20 +79,22 @@ int PhoneBook::get_amount()
 }
 void PhoneBook::search_print_contact(std::string i_to_search)
 {
+	int index;
+
 	if (this->get_amount() == 0)
 	{
 		std::cout << "\033[1;31mYou don't have any contacts!!\033[0m" << std::endl;
 		return ;
 	}
-	if (i_to_search != "")
-		std::cout << "Invalid index!" << std::endl;
-	// if (i_to_search > "8")
-	// {
-	// 	std::cout << "\033[1;31mThe " << i_to_search << "\033[1;31mth contact is not available. Please try a smaller number.\033[0m" << std::endl;
-	// 	return ;
-	// }
-	if (i_to_search.length() == 1 && i_to_search[0] >= '1' && i_to_search[0] <= '8' && \
-	this->contacts[i_to_search[0] - 1 - '0'].get_first_name().size())
+	if (i_to_search.empty() || ft_isdigit(i_to_search))
+		std::cout << "\031[1;31mInvalid index!" << std::endl;
+	index = my_stoi(i_to_search);
+	if (index > 8 || index < 1)
+	{
+		std::cout << "\033[1;31mThe " << i_to_search << "th contact is not available. Please try a smaller number.\033[0m" << std::endl;
+		return ;
+	}
+	if (this->contacts[index - 1].get_first_name().size())
 	{
 		std::cout << "SUCCESS" << std::endl;
 		// this->print_contact(i_to_search[0] - 1 - '0');
