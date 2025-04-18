95ffd192709596fb62157a17a715867961b06dc0
E1-Tamazyan <el.tamazyan03@gmail.com>
2025-04-18 21:11:48 +0400

    finished 02->ex02

---
+++ cpp_module_02/ex02/Fixed.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const int);
+		Fixed(const float);
+		Fixed(const Fixed &fixed);
+		Fixed& operator=(const Fixed& rhs);
+		~Fixed();
+	public:
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+		bool	operator>(Fixed rhs) const;
+		bool	operator>=(Fixed rhs) const;
+		bool	operator<(Fixed rhs) const;
+		bool	operator<=(Fixed rhs) const;
+		bool	operator==(Fixed rhs) const;
+		bool	operator!=(Fixed rhs) const;
+
+		const	Fixed	operator+(Fixed rhs) const;
+		const	Fixed	operator-(Fixed rhs) const;
+		const	Fixed	operator/(Fixed rhs) const;
+		const	Fixed	operator*(Fixed rhs) const;
+
+		Fixed&	operator++();
+		Fixed&	operator--();
+		const	Fixed	operator++(int);
+		const	Fixed	operator--(int);
+
+		static	Fixed &min(Fixed &first, Fixed &second);
+		static	const Fixed &min(Fixed const &first, Fixed const &second);
+		static	Fixed &max(Fixed &first, Fixed &second);
+		static	const Fixed &max(Fixed const &first, Fixed const &second);
+
+	private:
+		int num;
+		static const int fractional_bits = 8;
+		// static const int fractional_bits={8}; // can we do?
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
